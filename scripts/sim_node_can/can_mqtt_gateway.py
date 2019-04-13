e7a7be6b2cb71962e2c623a8fca40303cd1bd1ff
LJ <juranek.lukasz@gmail.com>
2019-04-13 13:49:39 +0200

    Sim update

--- scripts/sim_node_can/can_mqtt_gateway.py
+++ scripts/sim_node_can/can_mqtt_gateway.py
@@ -10,6 +10,20 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
+def commandFrameThread():
+    while(1):                
+        rx_message = can_bus.recv()
+        if (status_msg.frame_id == rx_message.arbitration_id):
+                print("CAN> ")
+                frame = db.decode_message(rx_message.arbitration_id, rx_message.data)
+                print(frame)
+                motorSetPoint = frame["RegulatorPosition"] 
+                client.publish("CAN/motorPosition", motorSetPoint)
+
+# can_bus = can.interface.Bus('/dev/ttyS7@115200', bustype='slcan')
+
+can_bus = can.interface.Bus('can0', bustype='socketcan')
+
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
@@ -17,21 +31,9 @@ client.on_message = on_message
 client.connect("localhost", 1883, 60)
 #client.loop_forever()
 
-
-
 db = cantools.database.load_file('motor/ucan.dbc')
 status_msg = db.get_message_by_name('DCControlFrame')
 command_msg = db.get_message_by_name('DCStatusFrame')
-
-def commandFrameThread():
-    while(1):                
-        rx_message = can_bus.recv()
-        if (status_msg.frame_id == rx_message.arbitration_id):
-            	print("CAN> ")
-            	frame = db.decode_message(rx_message.arbitration_id, rx_message.data)
-            	print(frame)
-            	motorSetPoint = frame["RegulatorPosition"] 
-		client.publish("CAN/motorPosition", motorSetPosition)
 
 w_rx = threading.Thread(target=commandFrameThread) 
 w_rx.start()
